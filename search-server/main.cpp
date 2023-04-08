160e1da63f5c101eb11e45e7711c274d1c0019d8
KartonArmadon <maxirykov@mail.ru>
2023-04-08 20:41:01 +0300

    271

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,49 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include <iostream>
+#include <string>
+using namespace std;
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+/// <summary>
+/// Считает количество чисел хотя бы с одной тройкой в диапазоне [a,b]. Границы включены.
+/// </summary>
+/// <param name="">Левая граница диапазона</param>
+/// <param name="">Правая граница диапазона</param>
+/// <returns>Количество чисел хотя бы с одной тройкой</returns>
+int CountThrees(int, int);
+
+int CountThrees1(int, int);
+
+int main() {
+	if (CountThrees(1, 1000) == 271)
+		cout << "Test passed :)" << endl;
+	
+	if (CountThrees1(1, 1000) == 271)
+		cout << "Test passed :)" << endl;
+}
+
+int CountThrees(int a, int b) {
+	int result = 0;
+	int temp;
+	for (int i = a; i <= b; ++i) {
+		temp = i;
+		while (temp != 0)
+		{
+			if (temp % 10 == 3){
+				cout << result << " "s << i << endl;
+				++result;
+				break;
+			}
+			temp /= 10;
+		}
+	}
+	return result;
+}
+
+int CountThrees1(int a, int b) {
+	int result = 0;
+	for (int i = a; i <= b; ++i) {
+		string temp = to_string(i);
+		if (temp.find("3") != string::npos)
+			++result;
+	}
+	return result;
+}
